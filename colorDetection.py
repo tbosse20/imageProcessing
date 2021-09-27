3fbc3406077415a9e7fda5b05eb611e94b02c22e
Tonko <tbosse20@student.aau.dk>
2021-09-27 14:56:52 +0200

    Video

--- colorDetection.py
+++ colorDetection.py
@@ -11,8 +11,8 @@ class Blob(Area):
         self.y = y
         self.w = 0
         self.h = 0
-        Blob.blobThreshold = 1
-        Blob.sizeFilter = 20
+        Blob.blobThreshold = 0
+        Blob.sizeFilter = 10
 
     def setThreshold(blobThreshold):
         Blob.blobThreshold = blobThreshold
@@ -87,25 +87,31 @@ def colorMask(img: np.ndarray, offset: float, lower: tuple, upper: tuple) -> np.
     return maskedImg
 
 
-def colorDetection(originalFrame: np.ndarray, blurLevel: int, offset: [int], lower: [int], upper: [int], type: str) -> [Blob]:
+def colorDetection(format: str, originalFrame: np.ndarray, blurLevel: int, offset: [int], lower: [int], upper: [int], type: str,
+                   lowerRes: int) -> [Blob]:
     # originalFrame = cv2.imread("", cv2.IMREAD_UNCHANGED)
     height, width, channels = originalFrame.shape
+
     imageProcess = originalFrame.copy()
 
     # TO-DO: LOWER/DECREASE RESOLUTION
+    # print(width)
+    imageProcess = cv2.resize(imageProcess, (int(width / lowerRes), int(height / lowerRes)))
+    # cv2.imshow('imageProcess', imageProcess)
+    # cv2.waitKey(0)
 
     # Blur image
     imageProcess = cv2.GaussianBlur(imageProcess, (blurLevel, blurLevel), 0)
-    cv2.imshow('Blur', imageProcess)
+    # cv2.imshow('Blur', imageProcess)
 
     # Mask water
     imageProcess = colorMask(imageProcess, offset, lower, upper)
-    cv2.imshow('Color mask', imageProcess)
+    # cv2.imshow('Color mask', imageProcess)
 
     # Canny Edge Detection
     imageProcess = cv2.cvtColor(imageProcess, cv2.COLOR_BGR2GRAY)
     imageProcess = cv2.Canny(imageProcess, 100, 200)
-    cv2.imshow('Edge detection', imageProcess)
+    # cv2.imshow('Edge detection', imageProcess)
 
     # Blob detect image
     blobs = BlobDetection(imageProcess, '0').getAreas()
@@ -115,20 +121,62 @@ def colorDetection(originalFrame: np.ndarray, blurLevel: int, offset: [int], low
     # Remove to small blobs
     for blob in list(blobs):
         if blob.w < Blob.sizeFilter or blob.h < Blob.sizeFilter:
-           blobs.remove(blob)
+            blobs.remove(blob)
 
     for blob in blobs:
         blob.type = type
 
+    for blob in blobs:
+        blob.x = blob.x * lowerRes
+        blob.y = blob.y * lowerRes
+        blob.w = blob.w * lowerRes
+        blob.h = blob.h * lowerRes
+
     imageProcess = drawRectangle(blobs, originalFrame, False)
+    # cv2.imshow('imageProcess', imageProcess)
+    # cv2.waitKey(1)
 
-    cv2.imshow('Find ' + type, imageProcess)
-    cv2.waitKey(0)
-    return blobs
+    if format == 'video':
+        return imageProcess
+    elif format == 'pic':
+        cv2.imshow('Find ' + type, imageProcess)
+        cv2.waitKey(0)
 
+def videoTest():
 
-#colorDetection(cv2.imread('TestImages/KingDomino1.jpg'), 9, (0, 0, 0), (100, 154, 28), (158, 255, 215), 'Water')
-colorDetection(cv2.imread('TestImages/KingDomino1.jpg'), 9, (0, 0, 0), (40, 0, 104), (72, 255, 212), 'Grass')
-#colorDetection(cv2.imread('TestImages/KingDomino1.jpg'), 9, (10, 0, 0), (43, 52, 12), (95, 238, 97), 'Forest')
-#colorDetection(cv2.imread('TestImages/KingDomino1.jpg'), 9, (0, 0, 0), (24, 68, 95), (36, 139, 171), 'Dirt')
-#colorDetection(cv2.imread('TestImages/Gloves1.png'), 9, (0, 0, 0), (48, 30, 104), (78, 122, 255), 'greenGlove')
+    cap = cv2.VideoCapture('TestImages/greensmall.mp4')
+
+    frameCount = 0
+
+    while (1):
+        # Capture frame-by-frame
+        _, frame = cap.read()
+        frame = cv2.resize(frame, (360, 640))
+
+        frameCount += 1
+        if frameCount % 5 == 0:
+            blurLevel = 9
+            offset = (10, 0, 0)
+            lower = (48, 30, 104)
+            upper = (78, 122, 255)
+            type = 'greenGlove'
+            lowerRes = 2
+            frame = colorDetection('video', frame, blurLevel, offset, lower, upper, type, lowerRes)
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(15) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+
+    cv2.destroyAllWindows()
+
+colorDetection('pic', cv2.imread('TestImages/KingDomino1.jpg'), 9, (0, 0, 0), (100, 154, 28), (158, 255, 215), 'Water', 1)
+#colorDetection('pic', cv2.imread('TestImages/KingDomino1.jpg'), 9, (0, 0, 0), (40, 0, 104), (72, 255, 212), 'Grass', 1)
+#colorDetection('pic', cv2.imread('TestImages/KingDomino1.jpg'), 9, (10, 0, 0), (43, 52, 12), (95, 238, 97), 'Forest', 1)
+#colorDetection('pic', cv2.imread('TestImages/KingDomino1.jpg'), 9, (0, 0, 0), (24, 68, 95), (36, 139, 171), 'Dirt', 1)
+#colorDetection('pic', cv2.imread('TestImages/Gloves1.png'), 9, (0, 0, 0), (48, 30, 104), (78, 122, 255), 'greenGlove', 1)
+#videoTest()
